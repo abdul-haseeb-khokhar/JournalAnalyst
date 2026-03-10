fa26862810355feef2daf028a143bbba597c2556
abdul-haseeb-khokhar <h.khokhar2015@gmail.com>
2026-03-10 10:33:20 +0500

    Addig JWT authentication

--- backend/services/user-services/controllers/userController.js
+++ backend/services/user-services/controllers/userController.js
@@ -1,5 +1,7 @@
 const userModel = require('../models/userModel');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 exports.register = async (req, res) => {
     try{
         const {name, email, password} = req.body;
@@ -9,5 +11,40 @@ exports.register = async (req, res) => {
         res.status(201).json({message: 'User created successfully ', user});
     } catch (error){
         res.status(500).json({message: error.message});
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const {email , password} = req.body;
+        const user = await userModel.findOne({ email});
+
+        if(!user){
+            return res.status(400).json({
+                message:"User not found"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({
+                message : "Invalid Credentials"
+            });
+        }
+
+        const token = jwt.sign(
+            {id : user._id},
+            process.env.JWT_SECRET,
+            {expiresIn: "7d"}
+        );
+
+        res.json({
+            message: "Login Successful",
+            token
+        })
+    } catch (error) {
+        res.status(500).json({
+            message : error.message
+        });
     }
 };
--- backend/services/user-services/routes/userRoutes.js
+++ backend/services/user-services/routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {register} = require('../controllers/userController');
+const userController = require('../controllers/userController');
 
-router.post('/register',register);
+router.post('/register',userController.register);
+router.post('/login', userController.login);
 
 module.exports = router;
