a5259d5e1e16d9f8ab77a71d3e3b8a1e081d8e46
abdul-haseeb-khokhar <h.khokhar2015@gmail.com>
2026-03-10 09:08:15 +0500

    ligin

--- backend/services/user-services/controllers/userController.js
+++ backend/services/user-services/controllers/userController.js
@@ -1,9 +1,11 @@
 const userModel = require('../models/userModel');
-
+const bcrypt = require('bcrypt');
 exports.register = async (req, res) => {
     try{
         const {name, email, password} = req.body;
-        const user = await userModel.create({name, email, password});
+
+        const hashedPassword = await bcrypt.hash(password,10);
+        const user = await userModel.create({name, email, password: hashedPassword});
         res.status(201).json({message: 'User created successfully ', user});
     } catch (error){
         res.status(500).json({message: error.message});
