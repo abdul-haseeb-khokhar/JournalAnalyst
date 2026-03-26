94d2fdacd7354e33eae2df35007ad28269add80c
abdul-haseeb-khokhar <h.khokhar2015@gmail.com>
2026-03-26 19:44:01 +0500

    Changing from loacal host to vercel domain

--- backend/services/user-services/server.js
+++ backend/services/user-services/server.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5001"],
+    origin: ["http://localhost:5173", "https://journal-analyst.vercel.app"],
     methods:["GET","POST","PUT","DELETE","OPTIONS"],
     allowedHeaders:["Content-Type","Authorization"],
     credentials:true
