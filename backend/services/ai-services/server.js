900d88d26016e224cfd22c68cc58f1dd3804ea22
abdullahyounasdev <ab.khokhar2005@gmail.com>
2026-04-01 17:27:31 +0500

    Adjusting cor to get data form vercel

--- backend/services/ai-services/server.js
+++ backend/services/ai-services/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:["http://localhost:5173", "http://localhost:5003"],
+    origin:["http://localhost:5173", "https://journal-analyst-ai-service.vercel.app"],
     methods: ["POST","GET", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials:true
